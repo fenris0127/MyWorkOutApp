78affdcae33f4011ef74259598016575772c1467
fenris0127 <77095756+fenris0127@users.noreply.github.com>
2026-05-20 18:50:02 +0900

    Update cached file from workout.html to index.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='workout-v3';
-const FILES=['./workout.html','./manifest.json'];
+const FILES=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
   self.skipWaiting();
